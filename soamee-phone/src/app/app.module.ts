bbbe7371f2564b461bc0d0d77c69be82b04bc185
VictorRascón <vrascon.sedano@gmail.com>
2021-09-27 13:18:19 +0200

    create components

--- soamee-phone/src/app/app.module.ts
+++ soamee-phone/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PhoneComponent } from './components/phone/phone.component';
+import { PhonesComponent } from './components/phones/phones.component';
+import { FormPhoneComponent } from './components/forms/form-phone/form-phone.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhoneComponent,
+    PhonesComponent,
+    FormPhoneComponent
   ],
   imports: [
     BrowserModule
---
+++ soamee-phone/src/app/components/forms/form-phone/form-phone.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-phone',
+  templateUrl: './form-phone.component.html',
+  styleUrls: ['./form-phone.component.css']
+})
+export class FormPhoneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
